54ff68fde8de8c350fca8f9717531ea2b930749a
Josh Zhang <joshzhang@berkeley.edu>
2024-02-19 23:15:55 -0800

    gru model done:

--- gru_model.py
+++ gru_model.py
@@ -19,7 +19,7 @@ class RobotData(Dataset):
                 label = bag[i + size][:6]       # Next state (excluding time / acceleration)
                 self.labels.append(label)
 
-        tens = lambda x: torch.tensor(x, dtype = torch.float64)
+        tens = lambda x: torch.tensor(x, dtype = torch.float32)
         self.featrs = tens(featrs)
         self.labels = tens(labels)
         if norm: self.norm()
@@ -39,34 +39,34 @@ class RobotData(Dataset):
     def __getitem__(self, i):
         return self.featrs[i], self.labels[i]
 
-
 class GRU_Model(nn.Module):
 
-    def __init__(self, inputs, hidden, output, layers, drop_r):
+    def __init__(self, inputs, hidden, output, layers, do_drop):
         super(GRU_Model, self).__init__()
 
         self.hidden = hidden
         self.layers = layers
+        dropout = 0.1 if do_drop else 0
+
         self.gru    = nn.GRU(inputs, hidden, layers,
-            batch_first=True, dropout=drop_r)
+            batch_first=True, dropout=dropout)
         self.full_c = nn.Linear(hidden, output)
 
     def forward(self, x):
-        # Init hidden state w/0           <BATCH_S>                  <CPU/GPU>
-        hidden = torch.zeros(self.layers, x.size(0), self.hidden).to(x.device)
+        # Init hidden state w/0           <BATCH_S>
+        hidden = torch.zeros(self.layers, x.size(0), self.hidden)
         # Forward propagate GRU
         output, _ = self.gru(x, hidden) # x/output: (BATCH_S, SEQ_LEN, IN/HID)
         # Decode last time step
         return self.full_c(output[:, -1, :]) # output shape: (BATCH_S, OUTPUT)
 
-
 INPUTS = 8      # (pos.(2), orient.(1), vel.(3), acceleration (2))
-HIDDEN = 128
 OUTPUT = 6      # (positions (2), orientation (1), velocities (3))
+HIDDEN = 64
 LAYERS = 8
-DROP_R = 0.1
+F_SIZE = 8
 
-F_SIZE  = 10
+DO_DROP = False
 DO_NORM = False
 BATCH_S = 128
 LEARN_R = 0.001
@@ -80,26 +80,23 @@ def train_model():
     v_size = len(dataset) - t_size
 
     t_data, v_data = random_split(dataset, [t_size, v_size])
-
     t_load = DataLoader(t_data, batch_size=BATCH_S, shuffle=True)
     v_load = DataLoader(v_data, batch_size=BATCH_S, shuffle=False)
 
-    criterion = nn.MSELoss()
-    model = GRU_Model(INPUTS, HIDDEN, OUTPUT, LAYERS, DROP_R)
-
+    model = GRU_Model(INPUTS, HIDDEN, OUTPUT, LAYERS, DO_DROP)
     optimizer = optim.Adam(model.parameters(), lr=LEARN_R)
     scheduler = ReduceLROnPlateau(optimizer)
 
+    criterion = nn.MSELoss()
     min_loss = float('inf')
     patience = 0
 
-    for epoch in range(EPOCHS):
+    for i in range(EPOCHS):
         model.train()
-
         for featrs, labels in t_load:
             optimizer.zero_grad()
-            output = model(featrs)
 
+            output = model(featrs)
             loss = criterion(output, labels)
             loss.backward()
             optimizer.step()
@@ -107,21 +104,29 @@ def train_model():
         scheduler.step()
         model.eval()
         v_loss = 0
-
         with torch.no_grad():
+
             for featrs, labels in v_load:
                 output = model(featrs)
                 loss = criterion(output, labels)
                 v_loss += loss.item()
         
         v_loss /= len(v_load)
-        print(f"Epoch {epoch+1}: valid_loss = {v_loss:.4f}")
+        print(f"Epoch {i + 1}: valid_loss = {v_loss:.4f}")
 
         if v_loss < min_loss:
             min_loss = v_loss
             patience = 0
+            path = f"models/GRU_{HIDDEN}_{LAYERS}_{F_SIZE}_{BATCH_S}_{v_loss}.pt"
+            torch.save(model.state_dict(), path)
 
         else: patience += 1
-        if patience >= 16:
-            print("Early stopping triggered.")
-            break
+        if patience >= 10: print("Early stop.") #; break
+
+if __name__ == "__main__":
+    for hidden in [64, 128]:
+        for layers in [8, 16]:
+            for f_size in [8, 16]:
+                for batch_s in [128, 1024]:
+                    HIDDEN, LAYERS, F_SIZE, BATCH_S = hidden, layers, f_size, batch_s
+                    train_model()
